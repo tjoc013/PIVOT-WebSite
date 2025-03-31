254084c13f16f3ded3a8fb24788d9aa7c2b74d31
Joaquin Sedamano <joaquinsedamano@MacBook-Pro-de-Joaquin.local>
2025-03-30 21:08:30 -0400

    More progress

--- src/data/routes.js
+++ src/data/routes.js
@@ -1,7 +1,7 @@
 const routes = [
   {
     index: true,
-    label: "Michael D'Angelo",
+    label: 'PIVOT @ VT',
     path: '/',
   },
   {
@@ -9,15 +9,15 @@ const routes = [
     path: '/about',
   },
   {
-    label: 'Resume',
-    path: '/resume',
-  },
-  {
     label: 'Projects',
     path: '/projects',
   },
   {
-    label: 'Stats',
+    label: 'Research',
+    path: '/resume',
+  },
+  {
+    label: 'null',
     path: '/stats',
   },
   {
