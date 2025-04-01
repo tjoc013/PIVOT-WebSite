d6291b616a3a3d813b8d8b9ea0462c6a0d739aea
Joaquin Sedamano <joaquin.sedamano@gmail.com>
2025-04-01 13:04:27 -0400

    Projects page updated

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,48 +1,38 @@
 // TODO Add a couple lines about each project
 const data = [
+  // {
+  //   title: 'Nearest Dollar',
+  //   subtitle: '2015 BVP Hackathon',
+  //   image: '/images/projects/nearestdollar.jpg',
+  //   date: '2015-11-20',
+  //   desc:
+  //     'Built for a social impact hackathon. '
+  //     + 'NearestDollar connected to your bank accounts, credit cards, '
+  //     + 'or debit cards and rounded up your purchases to donate the balance to '
+  //     + 'the charity of your choice.',
+  // },
   {
-    title: 'Nearest Dollar',
-    subtitle: '2015 BVP Hackathon',
-    image: '/images/projects/nearestdollar.jpg',
-    date: '2015-11-20',
+    title: 'Electric Field-Induced Dehumidifier',
+    subtitle: 'The first prototype is complete and we are excited to test it\'s effectiveness.',
+    link: 'https://www.linkedin.com/posts/pivotatvt_taking-steps-here-is-a-quick-update-on-activity-7311870367277404160-ljv1?utm_source=share&utm_medium=member_desktop&rcm=ACoAAC15nhEBqOiCdKwzipeaqnznC52CzrSnu-o',
+    image: '/images/projects/P1-1.jpg',
+    date: '2025-03-30',
     desc:
-      'Built for a social impact hackathon. '
-      + 'NearestDollar connected to your bank accounts, credit cards, '
-      + 'or debit cards and rounded up your purchases to donate the balance to '
-      + 'the charity of your choice.',
+      'After designing the circuit, modeling the apparatus in SolidWorks & 3D printing it, '
+      + 'and assembling everything in Virginia Tech\'s FRITH Lab the dehumidifier is nearly ready to go! '
+      + 'A more in-depth analysis and presentation is on its way and we can\'t wait to share this journey with everyone.',
   },
-  {
-    title: 'Harvest',
-    subtitle: 'Won 3rd. place in 2015 Techcrunch Disrupt SF Hackathon',
-    link: 'https://devpost.com/software/harvest',
-    image: '/images/projects/harvest.jpg',
-    date: '2015-09-20',
-    desc:
-      'Won ~ $7000 in prizes for an advanced, low cost monitoring solution '
-      + 'for crops. Harvest was designed to catch irrigation leaks, overwatering, '
-      + 'and nutrient deficiencies at an affordable price for the developing world.',
-  },
-  {
-    title: 'Space Potato',
-    subtitle: 'A kickstarter funded potato powered weather balloon.',
-    link: 'http://www.spacepotato.org',
-    image: '/images/projects/spacepotato.jpg',
-    date: '2015-06-28',
-    desc:
-      'Launched a potato battery powered weather balloon with two cameras '
-      + 'and gps transponder. Resulting photos were published in a coffee table book. '
-      + 'You can email me for a copy.',
-  },
-  {
-    title: 'Cat Detector',
-    subtitle: 'A convolutional neural network to classify cats! (and dogs)',
-    image: '/images/projects/catdetector.jpg',
-    date: '2015-05-15',
-    desc:
-      'Trained a convolutional neural network to classify between ~ 80 cats breeds. '
-      + 'Over 60,000 cats were classified before server bills made the project too expensive '
-      + 'to continue hosting.',
-  },
+  // {
+  //   title: 'Space Potato',
+  //   subtitle: 'A kickstarter funded potato powered weather balloon.',
+  //   link: 'http://www.spacepotato.org',
+  //   image: '/images/projects/spacepotato.jpg',
+  //   date: '2015-06-28',
+  //   desc:
+  //     'Launched a potato battery powered weather balloon with two cameras '
+  //     + 'and gps transponder. Resulting photos were published in a coffee table book. '
+  //     + 'You can email me for a copy.',
+  // },
 ];
 
 export default data;
