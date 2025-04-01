28539698cc2378d83c7baecd56ca06811dcd77ef
Joaquin Sedamano <joaquin.sedamano@gmail.com>
2025-04-01 13:05:29 -0400

    'Projects page updated'

--- src/data/projects.js
+++ src/data/projects.js
@@ -24,7 +24,7 @@ const data = [
   },
   // {
   //   title: 'Space Potato',
-  //   subtitle: 'A kickstarter funded potato powered weather balloon.',
+  //   subtitle: 'A kickstarter funded potato powered weather balloon.'
   //   link: 'http://www.spacepotato.org',
   //   image: '/images/projects/spacepotato.jpg',
   //   date: '2015-06-28',
